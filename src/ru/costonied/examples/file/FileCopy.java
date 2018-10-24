89582cb0f182bb1e1afe47c024b9e85a16581565
Igor Savin <costonied@gmail.com>
2018-10-24 08:03:33 +0300

    Initial Commit

---
+++ src/ru/costonied/examples/file/FileCopy.java
@@ -0,0 +1,30 @@
+package ru.costonied.examples.file;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * This class create input stream from File and output stream for another File.
+ * After it copy paste data from one to another.
+ */
+public class FileCopy
+{
+    public static void main(String[] args) throws IOException
+    {
+        String file1 = "/Users/igor/tmp/1.jpg";
+        String file2 = "/Users/igor/tmp/2.jpg";
+
+        FileInputStream fileInputStream = new FileInputStream(file1);
+        FileOutputStream fileOutputStream = new FileOutputStream(file2);
+
+        while (fileInputStream.available() > 0)
+        {
+            int data = fileInputStream.read();
+            fileOutputStream.write(data);
+        }
+
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
