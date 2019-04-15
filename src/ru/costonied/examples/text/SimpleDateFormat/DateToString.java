8bc504111930794f250d47a84e76b4533c1ba2a4
Игорь Савин <costonied@gmail.com>
2019-04-15 05:53:51 +0000

    Added first version of DateToString

---
+++ src/ru/costonied/examples/text/SimpleDateFormat/DateToString.java
@@ -0,0 +1,14 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Example - how to convert Date to String with needed format
+ */
+public class DateToString {
+    public static void main(String[] args) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        Date currentDate = new Date();
+        System.out.println("date: " + dateFormat.format(currentDate));
+    }
+}
