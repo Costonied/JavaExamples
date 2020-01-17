1575194b7feefc89461db15d01eb8bf150e0d2d5
Савин Игорь Владимирович <Savin.I.Vladi@sberbank.ru>
2020-01-17 08:10:34 +0300

    Added new example: Added cases of maths with Instant

--- src/main/java/ru/costonied/examples/dates/JDK8/InstantExample.java
+++ src/main/java/ru/costonied/examples/dates/JDK8/InstantExample.java
@@ -5,6 +5,8 @@ import java.time.Instant;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.UnsupportedTemporalTypeException;
 
 /**
  * Simple example showing some work with Instant class.
@@ -36,6 +38,24 @@ public class InstantExample {
         Duration duration = Duration.between(instantNow, instantLater);
 
         System.out.println("Duration between Instant objects in milliseconds: " + duration.toMillis());
+
+
+        /* Allowed mathematics with Instant objects */
+
+        Instant nextDay = instantNow.plus(1, ChronoUnit.DAYS);
+        Instant nextHour = instantNow.plus(1, ChronoUnit.HOURS);
+
+        System.out.println("Next day from now: " + nextDay);
+        System.out.println("Next hour from now: " + nextHour);
+
+        /* Not allowed mathematics with Instant objects */
+
+        try {
+            Instant nextYear = instantNow.plus(1, ChronoUnit.YEARS);
+            Instant nextMonth = instantNow.plus(1, ChronoUnit.MONTHS);
+        } catch (UnsupportedTemporalTypeException exception) {
+            System.out.println("Unfortunately, you can't moth Instant with YEARS and MONTHS ");
+        }
 
     }
 }
