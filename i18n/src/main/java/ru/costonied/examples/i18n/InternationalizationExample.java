5aef1c1a1df8cd69abbf2b8dd403be0b504ae9d6
Савин Игорь Владимирович <Savin.I.Vladi@sberbank.ru>
2020-01-20 09:47:20 +0300

    New example: Simple i18n example

---
+++ i18n/src/main/java/ru/costonied/examples/i18n/InternationalizationExample.java
@@ -0,0 +1,29 @@
+package ru.costonied.examples.i18n;
+
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Simple example shows how to do internationalization (i18n) using Java Resource Bundle files
+ */
+public class InternationalizationExample {
+    public static void main(String[] args) {
+
+        // Set several locales
+        Locale localeRu = Locale.getDefault();        // In Russia it "ru_RU"
+        Locale localeEn = new Locale("en");  // Just "en" without country is also valid value
+
+        // Get ResourceBundle for locales
+        ResourceBundle resourceBundleRu = ResourceBundle.getBundle("I18N", localeRu);
+        ResourceBundle resourceBundleEn = ResourceBundle.getBundle("I18N", localeEn);
+
+        String patternString = "Country is %s. City is %s";
+        String stringInRu = String.format(patternString, resourceBundleRu.getString("country"), resourceBundleRu.getString("city"));
+        String stringInEn = String.format(patternString, resourceBundleEn.getString("country"), resourceBundleEn.getString("city"));
+
+        System.out.println(stringInRu);
+        System.out.println(stringInEn);
+
+    }
+}
