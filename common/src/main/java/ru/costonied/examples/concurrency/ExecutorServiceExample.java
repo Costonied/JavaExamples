e2f59506d5ee084c539d7526fde0e3dd5d6b0b6b
Савин Игорь Владимирович <Savin.I.Vladi@sberbank.ru>
2020-02-12 08:01:57 +0300

    New example: Simple way of execute operations in new Thread using Concurrency API

---
+++ common/src/main/java/ru/costonied/examples/concurrency/ExecutorServiceExample.java
@@ -0,0 +1,30 @@
+package ru.costonied.examples.concurrency;
+
+
+import java.util.concurrent.Future;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * Simple example showing how to execute something in another Thread using Concurrency API
+ */
+public class ExecutorServiceExample {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        try {
+            /* One way of execute command in new thread is using execute() */
+            executorService.execute(
+                    () -> System.out.println("Hello! I'm new executed thread and my Thread is " + Thread.currentThread().getName()));
+
+            /* Another way is using submit() */
+            // Don't want to show here abilities of Future interface but want to show that you could save result of submit()
+            Future<?> future = executorService.submit(
+                    () -> System.out.println("Hello! I'm new submitted thread and my Thread is " + Thread.currentThread().getName()));
+        } finally {
+            executorService.shutdown();
+        }
+
+    }
+}
