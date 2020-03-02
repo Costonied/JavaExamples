e03276941ea50869402439eed5f572fb1b66af17
Савин Игорь Владимирович <Savin.I.Vladi@sberbank.ru>
2020-03-02 15:36:20 +0300

    New example: Using mark() and reset() in IO Streams

---
+++ common/src/main/java/ru/costonied/examples/io/streams/MarkResetExample.java
@@ -0,0 +1,33 @@
+package ru.costonied.examples.io.streams;
+
+
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * Show how to use mark() and reset() operations in IO Streams
+ */
+public class MarkResetExample {
+    public static void main(String[] args) throws IOException {
+
+        StringReader reader = new StringReader("ABCD");
+
+        System.out.print((char)reader.read());
+
+        if (reader.markSupported()) {
+
+            reader.mark(100);
+
+            System.out.print((char)reader.read());
+            System.out.print((char)reader.read());
+
+            reader.reset();
+            System.out.print(":reset:");
+
+            System.out.print((char)reader.read());
+            System.out.print((char)reader.read());
+            System.out.print((char)reader.read());
+        }
+
+    }
+}
