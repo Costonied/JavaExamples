d24c46d967f678b400039ae5599bf69380466290
Савин Игорь Владимирович <Savin.I.Vladi@sberbank.ru>
2020-03-05 08:36:33 +0300

    Updated example: finish example using ObjectInputStream and ObjectOutputStream

--- common/src/main/java/ru/costonied/examples/io/serialization/ObjectInputOutputStreamExample.java
+++ common/src/main/java/ru/costonied/examples/io/serialization/ObjectInputOutputStreamExample.java
@@ -1,7 +1,9 @@
 package ru.costonied.examples.io.serialization;
 
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import java.io.FileOutputStream;
@@ -15,26 +17,69 @@ import java.io.BufferedOutputStream;
  */
 public class ObjectInputOutputStreamExample {
 
-    public static void main(String[] args) throws IOException {
 
-        File outputFile = new File("/target/serialisedCat.txt");
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        Dog dog = new Dog(2, "Layka");
         Cat cat = new Cat(3, "Yasha");
+        File outputFileCat = new File("target/serialisedCat.txt");
+        File outputFileDog = new File("target/serialisedDog.txt");
 
-        serializeCatToFile(cat, outputFile);
+        serializeObjectToFile(cat, outputFileCat);
+        serializeObjectToFile(dog, outputFileDog);
+
+        Cat deserializedCat = deserializeObjectFromFile(Cat.class, outputFileCat);
+        Dog deserializedDog = deserializeObjectFromFile(Dog.class, outputFileDog);
+
+        System.out.println(deserializedCat);
+        System.out.println(deserializedDog);
 
     }
 
-    private static void serializeCatToFile(Cat cat, File outputFile) throws IOException {
+    /**
+     * Serialize any object to file
+     * @param object Any object
+     * @param outputFile destination file
+     * @throws IOException If something was wrong
+     */
+    private static void serializeObjectToFile(Object object, File outputFile) throws IOException {
         try (ObjectOutputStream outputStream = new ObjectOutputStream(
                 new BufferedOutputStream(new FileOutputStream(outputFile)))) {
-            outputStream.writeObject(cat);
+            outputStream.writeObject(object);
         }
+    }
+
+    /**
+     * Deserialize T object from file
+     * @param type type of object which deserialized
+     * @param inputFile file with serialized object
+     * @param <T> type of object which deserialized
+     * @return deserialized object
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private static <T> T deserializeObjectFromFile(Class<T> type, File inputFile) throws IOException,
+                                                                                    ClassNotFoundException {
+        Object deserializedObject;
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(
+                new BufferedInputStream(new FileInputStream(inputFile)))) {
+            deserializedObject = objectInputStream.readObject();
+        }
+
+        if (type.isInstance(deserializedObject)) {
+            return type.cast(deserializedObject);
+        }
+
+        return null;
     }
 
     /**
      * Inner class to serialize and deserialize
      */
     private static class Cat implements Serializable {
+
+        // Parameter which help check that serialize and deserialize use the same version of object
+        private static final long serialVersionUID = 1L;
 
         private int age;
         private String name;
@@ -47,6 +92,29 @@ public class ObjectInputOutputStreamExample {
         @Override
         public String toString() {
             return String.format("Cat name is %s and age is %d",
+                    this.name, this.age);
+        }
+    }
+
+    /**
+     * Inner class to serialize and deserialize
+     */
+    private static class Dog implements Serializable {
+
+        // Parameter which help check that serialize and deserialize use the same version of object
+        private static final long serialVersionUID = 1L;
+
+        private int age;
+        private String name;
+
+        public Dog(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Dog name is %s and age is %d",
                     this.name, this.age);
         }
     }
