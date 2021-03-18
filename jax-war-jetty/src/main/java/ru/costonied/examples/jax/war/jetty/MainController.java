c33b757258ae727e0a7829ad3ee09a9a39a6fe2f
Savin Igor <Savin.I.Vladi@sberbank.ru>
2021-03-18 22:33:56 +0300

    Added example how to read JNDI Resourse

--- jax-war-jetty/src/main/java/ru/costonied/examples/jax/war/jetty/MainController.java
+++ jax-war-jetty/src/main/java/ru/costonied/examples/jax/war/jetty/MainController.java
@@ -1,6 +1,8 @@
 package ru.costonied.examples.jax.war.jetty;
 
 import javax.ws.rs.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.core.Response;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -8,8 +10,12 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Path("/rest")
 public class MainController {
     @GET @Path("/check") @Produces(APPLICATION_JSON)
-    public Response getCheck() {
-        String body = "{\"status\":\"ok\"}";
+    public Response getCheck() throws NamingException {
+        // Read JNDI resource
+        InitialContext initContext = new InitialContext();
+        String statusCode = (String) initContext.lookup("some/resource/string");
+
+        String body = "{\"status\":\"" + statusCode + "\"}";
         return Response.ok(body).build();
     }
 }
