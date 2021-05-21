154b9b90391f2e5493e2630290632df288224d61
u16712687 <Savin.I.Vladi@sberbank.ru>
2021-05-21 23:57:01 +0300

    Apache POI example. Read PPTX presentation and delete several slides
    * Example with deletion one slide was renamed

--- apache-poi/src/main/java/ru/savini/apache/poi/pptx/ReadPresentationAndDeleteOneSlideExample.java
+++ apache-poi/src/main/java/ru/savini/apache/poi/pptx/ReadPresentationAndDeleteOneSlideExample.java
@@ -6,7 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class ReadPresentationAndDeleteSlideExample {
+public class ReadPresentationAndDeleteOneSlideExample {
     public static void main(String[] args) throws IOException {
         try (XMLSlideShow ppt = new XMLSlideShow(new FileInputStream("./tmp/slideshow.pptx"));
              FileOutputStream out = new FileOutputStream("./tmp/new-slideshow.pptx")) {
