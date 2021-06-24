d81b4b2ac99448e5524ce41ed98225c60dd11615
u16712687 <Savin.I.Vladi@sberbank.ru>
2021-06-24 10:54:45 +0300

    Apache POI. Correction for slide deletes

--- apache-poi/src/main/java/ru/savini/apache/poi/pptx/ReadPresentationAndDeleteSeveralSlidesExample.java
+++ apache-poi/src/main/java/ru/savini/apache/poi/pptx/ReadPresentationAndDeleteSeveralSlidesExample.java
@@ -1,6 +1,9 @@
 package ru.savini.apache.poi.pptx;
 
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFShape;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -16,9 +19,27 @@ public class ReadPresentationAndDeleteSeveralSlidesExample {
             slidesForDeleteIndexes.add(3);
             Iterator<Integer> slidesForDelete = slidesForDeleteIndexes.descendingIterator();
             while (slidesForDelete.hasNext()) {
-                ppt.removeSlide(slidesForDelete.next());
+                int slideIndex = slidesForDelete.next();
+                XSLFSlide slide = ppt.getSlides().get(slideIndex);
+                deleteSlideShapes(slide);
+                ppt.removeSlide(slideIndex);
             }
             ppt.write(out);
+        }
+    }
+
+    /**
+     * @implNote Удаление форм со слайдов перед удалением самого слайда
+     * необходимо, чтобы уменьшить размер итоговой презентации, т.к.
+     * некоторых версиях poi-ooxml замечено, что размер итогового файла
+     * не уменьшается после удаления слайдов из-за того, что формы
+     * записываются в итоговый файл хотя их и не видно
+     */
+    private static void deleteSlideShapes(XSLFSlide slide) {
+        int countOfShapes = slide.getShapes().size();
+        for (int i = countOfShapes - 1; i >= 0; i--) {
+            XSLFShape shape = slide.getShapes().get(i);
+            slide.removeShape(shape);
         }
     }
 }
