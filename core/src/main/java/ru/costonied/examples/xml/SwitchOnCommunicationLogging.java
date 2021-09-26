a1d4cfa22c2b1feabed1a6aac3898b1a5bce4b44
u16712687 <Savin.I.Vladi@sberbank.ru>
2021-09-26 21:50:15 +0300

    XML. Debug communications dump

---
+++ core/src/main/java/ru/costonied/examples/xml/SwitchOnCommunicationLogging.java
@@ -0,0 +1,24 @@
+package ru.costonied.examples.xml;
+
+/**
+ * The example showing how to switch on communication dump logging to console.
+ * Useful for example if you want to see Request/Response for SOAP service.
+ *
+ * @implSpec Also you could use this parameters as command line
+ * parameter (using -D or as environment variable):
+ *  -Dcom.sun.xml.ws.transport.http.client.HttpTransportPipe.dump=true
+ *  -Dcom.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump=true
+ *  -Dcom.sun.xml.ws.transport.http.HttpAdapter.dump=true
+ *  -Dcom.sun.xml.internal.ws.transport.http.HttpAdapter.dump=true
+ *  -com.sun.xml.internal.ws.transport.http.HttpAdapter.dumpTreshold=999999
+ *
+ */
+public class SwitchOnCommunicationLogging {
+    public static void main(String[] args) {
+        System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dumpTreshold", "999999");
+    }
+}
