42c0bd2b71ff813d60ac5f7fa0afe70457d7ed38
u16712687 <Savin.I.Vladi@sberbank.ru>
2022-01-25 09:56:21 +0300

    Embedded Jetty Server and Servlets
    * Working blocking servlet in Jetty
    * Working non blocking (Async) servlet in Jetty

---
+++ jetty/src/main/java/ru/costonied/examples/jetty/servlet/BlockingServlet.java
@@ -0,0 +1,16 @@
+package ru.costonied.examples.jetty.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class BlockingServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("{ \"status\": \"ok\"}");
+    }
+}
